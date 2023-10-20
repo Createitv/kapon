9d56d8de6786882e645231b3e585d9152635587a
Createitv <xfy150150@163.com>
2023-10-20 17:36:14 +0800

    更改excel操作库为exceljs

--- frontend/src/common/file.ts
+++ frontend/src/common/file.ts
@@ -1,25 +1,25 @@
+/** @format */
+
 import * as XLSX from 'xlsx'
 
 export function dataToExcel(data: any, filename: string) {
-  // 函数实现，参数 delay 单位 毫秒 ；
-  const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data)
-  const workbook: XLSX.WorkBook = XLSX.utils.book_new()
-  XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
-  XLSX.writeFileXLSX(workbook, filename + '.xlsx', {
-    type: 'buffer',
-    bookType: 'xlsx',
-  })
+	// 函数实现，参数 delay 单位 毫秒 ；
+	const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(data)
+	const workbook: XLSX.WorkBook = XLSX.utils.book_new()
+	XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1')
+	XLSX.writeFileXLSX(workbook, filename + '.xlsx', {
+		type: 'buffer',
+		bookType: 'xlsx',
+	})
 }
 
 export function ex() {
-  dataToExcel(d, '测试文件')
-  console.log('test excel done')
+	console.log('test excel done')
 }
 
-
 export function readCompareExcel(path: string) {
-  const workbook = XLSX.read(path, {})
-  const worksheet = workbook.Sheets[workbook.SheetNames[0]]
-  const raw_data = XLSX.utils.sheet_to_json(worksheet, {header: 1})
-  console.log(raw_data)
+	const workbook = XLSX.read(path, {})
+	const worksheet = workbook.Sheets[workbook.SheetNames[0]]
+	const raw_data = XLSX.utils.sheet_to_json(worksheet, { header: 1 })
+	console.log(raw_data)
 }
